f98d096dc686f7c38e9c1627707f5043f3f7ef40
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-08-04 11:04:40 +0300

    Update express.js

--- server/config/express.js
+++ server/config/express.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
     if (config.PROD) {
         app.use(express.static(path.join(__dirname, "client/dist/PC-World")));
         app.get("*", (req, res) => {
-            res.sendFile(path.join(__dirname, "client/dist/PC-World", 'index.html'));
+            res.sendFile(path.join(__dirname, "client/PC-World/dist/PC-World", 'index.html'));
         });
     }
     app.use(cors(config.CORS));
