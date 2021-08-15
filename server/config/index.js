808fead3300b91bab51849a34273d9104e00e5c7
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-08-15 15:51:24 +0300

    Added comments referencing environment

--- server/config/index.js
+++ server/config/index.js
@@ -3,14 +3,14 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         PORT: process.env.PORT || 3000,
-        DB_CONNECTION: process.env.DB_CONNECTION_PC_WORLD,
+        DB_CONNECTION: process.env.DB_CONNECTION_PC_WORLD, // enter your local MongoDB string here
         COOKIE_NAME: 'X-Authorization',
         SECRET: 'very strong secret',
         SALT_ROUNDS: 10,
         CLOUDINARY: {
-            cloud_name: process.env.CLOUDINARY_NAME,
-            api_key: process.env.CLOUDINARY_KEY,
-            api_secret: process.env.CLOUDINARY_SECRET
+            cloud_name: process.env.CLOUDINARY_NAME, // enter your cloud name from cloudinary account
+            api_key: process.env.CLOUDINARY_KEY, // enter your cloud key from cloudinary account
+            api_secret: process.env.CLOUDINARY_SECRET // enter your cloud secret from cloudinary account
         },
         CORS: {
             origin: ['http://localhost:4200'],
